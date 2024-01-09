b530a5d3744f58bd2d54e2f85b202edc45d891b2
unknown <ann.zirinova@mail.ru>
2024-01-09 15:44:41 +0300

    first

---
+++ app/src/main/java/com/example/vsuwt_enrollee/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.vsuwt_enrollee.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
